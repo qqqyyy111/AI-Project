b2a6c0dab945b323f0952d016bc9071b7021cd5c
qiuyu <55216735+qqqyyy111@users.noreply.github.com>
2020-02-14 01:54:02 -0500

    Update Astar.java

--- src/gwu/zqy/main/Astar/Astar.java
+++ src/gwu/zqy/main/Astar/Astar.java
@@ -1,26 +1,17 @@
 package gwu.zqy.main.Astar;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 // f(n) = g(n) + h(n)
-// H(n) = max { abs(current_cell.x ¨C goal.x), abs(current_cell.y ¨C goal.y) } 
+// H(n) = max { abs(current_cell.x – goal.x), abs(current_cell.y – goal.y) } 
 
 
 public class Astar {
  
 	  public static int INF = Integer.MAX_VALUE;
 	  public static int squareSize = 10;
-	  public static int[][] vertices = new int[2000][10];
+	  public static int[][] vertices = new int[2000][3];
 	  public static int[][] edges = new int[2000][2000];
 	  
 	public static int aStar(int source, int target, int[][] vertices, int[][] edges) {
@@ -37,7 +28,7 @@ public class Astar {
 			      }
 			   });
 	   
-			   //find the optimal point
+			   //find the optimal point from 
 			   Set<Integer> parent = new HashSet<Integer>();
 			   parent.add(source);
 			   Vertex ver = new Vertex(source, HDist(source, target, vertices)+0, 0, parent);
@@ -56,60 +47,83 @@ public class Astar {
 		       parents.add(i);
 		       Vertex ver1 = new Vertex(i, g+h, g, parents);
 		       queue.add(ver1);
-		      }
+		       }
 		   }
-		     ver = queue.peek();
-		     source = ver.name;
-		     result = ver.g;
-		     parent = ver.parents;
+		       ver = queue.peek();
+		       source = ver.name;
+		       result = ver.g;
+		       parent = ver.parents;
 	}
 		   return result;
 }
 
 	
   	public static void readFile1() throws IOException, FileNotFoundException {
-   
-			   File file1 = new File("graphs/graph200_0.1/v.txt");
-			   FileInputStream inputStream1 = new FileInputStream(file1);
-			   BufferedReader bufferedReader1 = new BufferedReader(new InputStreamReader(inputStream1));
-			   String str1 = null;
-			   int vertexCount = 0;
-			   while((str1 = bufferedReader1.readLine()) != null) {
+  
+	   	    File file1 = new File("graphs/graph200_0.1/v.txt");
+	  	    FileInputStream inputStream1 = new FileInputStream(file1);
+		    BufferedReader bufferedReader1 = new BufferedReader(new InputStreamReader(inputStream1));
+		  	    	  	
+		    FileReader fileReader = new FileReader(file1); 	          
+  	            LineNumberReader lineNumberReader = new LineNumberReader(fileReader);
+  	            lineNumberReader.skip(Long.MAX_VALUE);
+  	            long lines = lineNumberReader.getLineNumber() + 1;
+  	            //System.out.print(lines);
+  	            lineNumberReader.close();		
+		  	            
+  	            String str1 = null;
+	            int vertexCount = 0;
+	            while((str1 = bufferedReader1.readLine()) != null) {
 			    //read Square from the graph
-			    int i = Integer.parseInt(str1.split(",")[1]); 
-			    vertices[vertexCount][0] = i;
-			    vertexCount++;
-			   }
-			   
-			   for(int m=0; m<edges.length; m++) {
-			     for(int n=0; n<500; n++) {
-			       if(m != n)
-			       edges[m][n] = INF;
-			     else
-			       edges[m][n] = 0;
-			   }
-			  }
-			   inputStream1.close();
-			   bufferedReader1.close();
-			 }
-
+	            String[] num = str1.split(",");
+	            while(vertexCount < lines & vertexCount > 0){
+	            	vertices[vertexCount][0] = Integer.parseInt(num[1]);
+	            	vertexCount ++;
+	            }   
+	            
+}
+	            for(int m=0; m<edges.length; m++) {
+				    for(int n=0; n<20; n++) {
+					   if(m != n)
+						   edges[m][n] = INF;
+					   else
+						   edges[m][n] = 0;
+				   	}
+	}       	
+			inputStream1.close();
+			bufferedReader1.close();
+		  	            
+}
 
   	public static void readFile2() throws NumberFormatException, IOException {
-    
-			    File file2 =new File("graphs/graph200_0.1/e.txt");
-			    FileInputStream inputStream2 = new FileInputStream(file2);
-			    BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(inputStream2));
-			    String str2 = null;
-			    while((str2 = bufferedReader2.readLine()) != null) {
-			     //read from Index, to Index, pathCost from graph
-			     int i = Integer.parseInt(str2.split(",")[0]);
-			     int j = Integer.parseInt(str2.split(",")[1]);
-			     int dist = Integer.parseInt(str2.split(",")[2]);
-			     edges[i][j]=dist;
-			     edges[j][i]=dist;
-			    }
-			    inputStream2.close();
-			    bufferedReader2.close();
+  				
+	  		BufferedReader reader;
+			try {
+				String encoding = "UTF-8"; 
+				File file2 = new File("graphs/graph200_0.1/e.txt");
+				if (file2.isFile() && file2.exists()) {       	// if the file exists
+				InputStreamReader read = new InputStreamReader(new FileInputStream(file2), encoding);
+				
+				//input the file name
+				reader = new BufferedReader(new FileReader(file2)); 
+	            String line = reader.readLine();
+	            line = reader.readLine();
+				while (line != null) { 
+					String[] num = line.split(",");
+					
+	                edges[Integer.parseInt(num[0])][Integer.parseInt(num[1])]
+	                            = Integer.parseInt(num[2]);
+	                edges[Integer.parseInt(num[1])][Integer.parseInt(num[0])]
+	                        = Integer.parseInt(num[2]);
+	                line = reader.readLine();
+				}
+				read.close();
+			}
+		}
+				catch (IOException e) {
+		            e.printStackTrace();
+				}
+	
 }
 			 
 
@@ -127,7 +141,7 @@ public class Astar {
 			   Scanner in = new Scanner(System.in);
 			   System.out.println("Please input the source vertex: ");
 			   int source = in.nextInt();
-			   System.out.println("Please input the source vertex: ");
+			   System.out.println("Please input the target vertex: ");
 			   int target = in.nextInt();
 			   
 			   int result = aStar(source, target, vertices, edges);
